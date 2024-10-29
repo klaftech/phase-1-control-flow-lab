26c308f4245752abc9c25d447b8fdf59c90878ff
Elchonon <ekn400@gmail.com>
2024-10-29 10:00:16 -0400

    lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance < 400){
+    return "This one is on me!"
+  } else if(distance > 400 && distance < 2000) {
+    return "That will be twenty bucks."
+  } else if(distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  switch(city){
+      case "NYC":
+        return "Ok, sounds good."
+        break;
+      case "Pittsburgh":
+        return "No go."
+        break;
+      default:
+        return city
+  }
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === "generous"){
+    return "Thank you so much."
+  } else if(tip === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
